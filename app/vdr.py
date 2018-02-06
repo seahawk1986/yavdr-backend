6a1b92f5d33ccf3682bba806ec87b713968a6535
Alexander Grothe <seahawk1986@gmx.de>
2018-02-06 10:12:56 +0100

    Add add raw timer string to dictionary

--- app/vdr.py
+++ app/vdr.py
@@ -43,8 +43,8 @@ class VDR_Timers(Resource):
         t_data = []
         for t in timers:
             status, channel, day, start, stop, priority, lifetime, filename, aux = t.split(':')
-            t_data.append({"status": int(status), "channel": channel, "day": day,
-                           "start": int(start), "stop": int(stop), "priority": int(priority), "lifetime": lifetime, "filename": filename, "aux": aux})
+            t_data.append({"raw": t, "status": int(status), "channel": channel, "day": day,
+                           "start": int(start), "stop": int(stop), "priority": int(priority), "lifetime": int(lifetime), "filename": filename, "aux": aux})
         return t_data
 
 class VDR_Channels(Resource):
