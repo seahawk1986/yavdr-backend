d6737494053641c8a30771ae5dd61a3ee27a5af6
Alexander Grothe <seahawk1986@gmx.de>
2018-02-19 11:24:09 +0100

    add some resources to call the DBus backend for actions

--- app/api_collection.py
+++ app/api_collection.py
@@ -4,11 +4,48 @@ from gi.repository import GLib
 from flask import request
 from flask_restful import Resource
 import system_utils.usage as sys_usage
+import system_utils.dbus as dbus_tools
 from . import auth as pam_auth
+
+system_bus = pydbus.SystemBus()
 
 class SystemInfo(Resource):
     def get(self):
-        return sys_usage.collect_data()
+        return sys_usage.collect_data(), 200
+
+class SystemAction(Resource):
+    def post(self, action_name):
+        try:
+            backend = system_bus.get('de.yavdr.backend')
+            data = request.get_json()
+            if data is None:
+                data = {}
+            elif not isinstance(data, dict):
+                return {'msg': 'you must pass a json object ("{}")' }, 402
+
+            data = { key: dbus_tools.av(value) for key, value in data.items()}
+            print(data)
+            r_code, r_msg = backend.queue_action(action_name, data)
+            if r_code == 200:
+                return {'msg': 'action queued', 'job_id': r_msg }, r_code
+            else:
+                return {'msg': 'action rejected: {}'.format(r_msg)}, r_code
+        except GLib.Error:
+            return {'msg': 'yavdr-backend is not available'}, 503
+
+class JobStatus(Resource):
+    def get(self, job_id):
+        try:
+            backend = system_bus.get('de.yavdr.backend')
+            response = backend.jobStatus(job_id)
+            return {'msg': 'job is {}'.format(job_str),
+                    'is_running': job_is_running,
+                    'is_cancelled': 'job_is_cancelled',
+                    'is_done': job_is_done,
+            }, 200
+        except GLib.Error:
+            return {'msg': 'yavdr-backend is not available'}, 503
+
 
 class VDR_Status(Resource):
     def get(self):
