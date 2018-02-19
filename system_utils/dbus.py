1427a049d7e049b4b5e46c72d505af2986e1238e
Alexander Grothe <seahawk1986@gmx.de>
2018-02-19 11:20:44 +0100

    Add auto variant function to make pydbus calls easier

---
+++ system_utils/dbus.py
@@ -0,0 +1,70 @@
+from functools import singledispatch
+from gi.repository import GLib
+
+"""
+The function av(arg) (short for auto variant) changes usual data types
+to dbus variants. This is especially useful when using pydbus for api calls
+which allow a great variety of arguments.
+"""
+
+@singledispatch
+def av(arg):
+    raise NotImplementedError('Unsupported type')
+
+@av.register(int)
+def _(arg):
+    return GLib.Variant('i', arg)
+
+@av.register(str)
+def _(arg):
+    return GLib.Variant('s', arg)
+
+@av.register(bool)
+def _(arg):
+    return GLib.Variant('b', arg)
+
+@av.register(list)
+def _(arg):
+    if all(map(lambda x: isinstance(x, str), arg)):
+        variant_type = 'as'
+    elif all(map(lambda x: isinstance(x, int), arg)):
+        variant_type = 'ai'
+    elif all(map(lambda x: isinstance(x, bool), arg)):
+        variant_type = 'ab'
+    else:
+        arg = [av(a) for a in arg]
+        variant_type = 'av'
+    return GLib.Variant(variant_type, arg)
+
+@av.register(dict)
+def _(arg):
+    if all(map(lambda x: isinstance(x, str), arg.keys())):
+        variant_type_keys = "s"
+    elif all(map(lambda x: isinstance(x, int), arg.keys())):
+        variant_type_keys = "i"
+    elif all(map(lambda x: isinstance(x, bool), arg.keys())):
+        variant_type_keys = "b"
+    else:
+        variant_type_keys = "v"
+
+    if all(map(lambda x: isinstance(x, str), arg.values())):
+        variant_type_values = "s"
+    elif all(map(lambda x: isinstance(x, int), arg.values())):
+        variant_type_values = "i"
+    elif all(map(lambda x: isinstance(x, bool), arg.values())):
+        variant_type_values = "b"
+    else:
+        variant_type_values = "v"
+
+    variant_signature = "a{%s%s}" % (variant_type_keys, variant_type_values)
+
+    ret_args = {}
+    for key, value in arg.items():
+        if variant_type_keys == "v":
+            key = av(key)
+        if variant_type_values == "v":
+            value = av(value)
+        ret_args[key] = value
+    return GLib.Variant(variant_signature, ret_args)
+
+
