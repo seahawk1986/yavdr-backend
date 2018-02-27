04ddcc66994be9dc8ae76623ad14926af4edb3b7
Alexander Grothe <seahawk1986@gmx.de>
2018-02-27 11:27:22 +0100

    function argument must be "task", not "task_id

--- app/api_collection.py
+++ app/api_collection.py
@@ -1,5 +1,6 @@
 import time
 import pydbus        
+from functools import wraps
 from gi.repository import GLib
 from flask import request
 from flask_restful import Resource
@@ -9,52 +10,57 @@ from . import auth as pam_auth
 
 system_bus = pydbus.SystemBus()
 
+def pydbus_error_handler(name="service"):
+    def decorator(f):
+        @wraps(f)
+        def wrapper(*args, **kwds):
+            try:
+                return f(*args, **kwds)
+            except GLib.Error:
+                return {'msg': '{} is not available'.format(name)}, 503
+        return wrapper
+    return decorator
+
 class SystemInfo(Resource):
     def get(self):
         return sys_usage.collect_data(), 200
 
 class SystemTask(Resource):
+    @pydbus_error_handler(name='yavdr-backend')
     def post(self, task):
-        try:
-            backend = system_bus.get('de.yavdr.backend')
-            data = request.get_json()
-            if data is None:
-                data = {}
-            elif not isinstance(data, dict):
-                return {'msg': 'you must pass a json object ("{}")' }, 402
+        backend = system_bus.get('de.yavdr.backend')
+        data = request.get_json()
+        if data is None:
+            data = {}
+        elif not isinstance(data, dict):
+            return {'msg': 'you must pass a json object ("{}")' }, 402
 
-            data = { key: dbus_tools.av(value) for key, value in data.items()}
-            r_code, r_msg = backend.queue_task(task, data)
-            if r_code == 200:
-                return {'msg': 'task queued', 'task_id': r_msg }, r_code
-            else:
-                return {'msg': 'task rejected: {}'.format(r_msg)}, r_code
-        except GLib.Error:
-            return {'msg': 'yavdr-backend is not available'}, 503
+        data = { key: dbus_tools.av(value) for key, value in data.items()}
+        r_code, r_msg = backend.queue_task(task, data)
+        if r_code == 200:
+            return {'msg': 'task queued', 'task_id': r_msg }, r_code
+        else:
+            return {'msg': 'task rejected: {}'.format(r_msg)}, r_code
 
-    def get(self, task_id):
-        try:
-            backend = system_bus.get('de.yavdr.backend')
-            r_code, data = backend.taskStatus(task_id)
-            # TODO: get task status from response
-            if r_code != 200:
-                return {'msg': 'invalid task id'}, r_code
-            return {'msg': 'task is {}'.format(data.get("msg")),
-                    'is_running': data.get('is_running'),
-                    'is_cancelled': data.get('is_cancelled'),
-                    'is_done': data.get('is_done'),
-            }, 200
-        except GLib.Error:
-            return {'msg': 'yavdr-backend is not available'}, 503
+    @pydbus_error_handler(name='yavdr-backend')
+    def get(self, task):
+        backend = system_bus.get('de.yavdr.backend')
+        r_code, data = backend.taskStatus(task)
+        # TODO: get task status from response
+        if r_code != 200:
+            return {'msg': 'invalid task id'}, r_code
+        return {'msg': 'task is {}'.format(data.get("msg")),
+                'is_running': data.get('is_running'),
+                'is_cancelled': data.get('is_cancelled'),
+                'is_done': data.get('is_done'),
+        }, 200
 
-    def delete(self, task_id):
-        try:
-            backend = system_bus.get('de.yavdr.backend')
-            r_code, msg = backend.deleteTask(task_id)
-            # TODO: get task status from response
-            return {'msg': msg}, r_code
-        except GLib.Error:
-            return {'msg': 'yavdr-backend is not available'}, 503
+    @pydbus_error_handler(name='yavdr-backend')
+    def delete(self, task):
+        backend = system_bus.get('de.yavdr.backend')
+        r_code, msg = backend.deleteTask(task)
+        # TODO: get task status from response
+        return {'msg': msg}, r_code
         
 
 
