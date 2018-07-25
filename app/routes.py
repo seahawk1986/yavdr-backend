8f73c2bca039cb650d571097720016c3040c661e
Alexander Grothe <seahawk1986@gmx.de>
2018-07-25 09:25:25 +0200

    cleanup imports in routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,18 +1,12 @@
 import os
-from app import app, api
-from flask import render_template, request, session, redirect, url_for, jsonify, send_from_directory
-from flask_restful import Resource, Api
-from itsdangerous import (TimedJSONWebSignatureSerializer
-                          as Serializer, BadSignature, SignatureExpired)
-from gi.repository import GLib
+from flask import render_template, session, redirect, url_for, jsonify, send_from_directory
 
-import system_utils.pam as pam
+from app import app, api
 import system_utils.usage as sys_usage
 from . import auth as pam_auth
 from . import api_collection as api_col
 from . import vdr
 
-import pydbus
 
 app.secret_key = os.urandom(24)
 
