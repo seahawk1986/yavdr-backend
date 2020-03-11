984e730adbaf0d0772b6812d7d65fa48bd705fe2
Alexander Grothe <seahawk1986@gmx.de>
2020-03-11 16:45:27 +0100

    dark colorschema, optimize meters

--- app/static/js/login.js
+++ app/static/js/login.js
@@ -1,80 +1,58 @@
 "use strict";
 
-var token_data = null;
-var modal_login_instance;      // the login dialog
-var modal_connection_instance; // the connection lost dialog (TODO)
+var auth; // our Authorization instance
 
-function logout() {
-    console.log("called logout")
-    token_data = null;
-    modal_login_instance.open();
-}
-
-function handleHTTPErrors(response) {
-    if (!response.ok) {
-        throw Error(response.statusText);
-    }
-    return response.json();
-}
-
-async function authenticated_http_request(url, method="GET", request_data={}) {
-    if (token_data === null) throw "token is null";
-    const myHeaders = new Headers();
-    myHeaders.append("accept", "application/json");
-    myHeaders.append("Authorization", `Bearer ${token_data.access_token}`)
-    let data = fetch(url,
-        {
-            method: method,
-            mode: 'cors',
-            cache: 'default',
-            headers: myHeaders,
-            body: (method.toUpperCase() == "GET") ? null : JSON.stringify(request_data),
+class Authorization {
+    constructor() {
+        // initialize the modal login dialog
+        var elems = document.querySelectorAll('#modal-login');
+        var options = {
+            "dismissible": false,
         }
-    ).then(response => {
-            if (!response.ok) {
-                throw Error(response.statusText);
-            }
-            return response.json();
+        var instances = M.Modal.init(elems, options);
+
+        // set up variables
+        this.token_data = null;
+        this.sys_stat_timer;
+        this.modal_login_instance = M.Modal.getInstance(document.getElementById("modal-login"));
+        this.loginForm = document.getElementById('login_form');
+        this.login_msg = document.getElementById('login_msg');
+        this.login_progress = document.getElementById('login_progress');
+        this.login_btn = document.getElementById('login_btn');
+
+
+
+
+        if (this.token_data === null) {
+            this.modal_login_instance.open();
+            const login_username = document.getElementById('login_username');
+            login_username.focus();
         }
-    ).catch(err => {
-            console.error("http request failed:", err);
-            throw err;
-        })
-    console.log("end of authenticated http request")
-    return data;
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-    M.AutoInit();
-
-    var elems = document.querySelectorAll('#modal-login');
-    var options = {
-        "dismissible": false,
     }
-    var instances = M.Modal.init(elems, options);
 
-    modal_login_instance = M.Modal.getInstance(document.getElementById("modal-login"));
-    const loginForm = document.getElementById('login_form');
-    const login_msg = document.getElementById('login_msg');
-    const login_progress = document.getElementById('login_progress');
-    const login_btn = document.getElementById('login_btn');
-
-    if (token_data === null) {
-        modal_login_instance.open();
-        const login_username = document.getElementById('login_username');
-        login_username.focus();
+    handleHTTPErrors(response) {
+        if (!response.ok) {
+            throw Error(response.statusText);
+        }
+        return response.json();
     }
-    loginForm.addEventListener('submit', function (e) {
-        e.preventDefault();
-        // console.log("Login button clicked...")
-        login_btn.disabled = true;
-        login_msg.textContent = "";
-        login_progress.innerHTML = '<div class="progress"><div class="indeterminate" style="width: 0%"></div></div>'
+
+    logout() {
+        console.log("called logout")
+        this.token_data = null;
+        this.modal_login_instance.open();
+        clearInterval(this.sys_stat_timer);
+    }
+
+    login(form) {
+        this.login_btn.disabled = true;
+        this.login_msg.textContent = "";
+        this.login_progress.innerHTML = '<div class="progress"><div class="indeterminate" style="width: 0%"></div></div>'
 
         const myHeaders = new Headers();
         myHeaders.append("accept", "application/json");
+        const formData = new FormData(form);
 
-        const formData = new FormData(this);
 
         // TODO: remove this debug output
         // for (var [key, value] of formData.entries()) { 
@@ -95,58 +73,185 @@ document.addEventListener('DOMContentLoaded', function () {
                 })
             .then(
                 (data) => {
-                    // console.log("got data:", data);
-                    token_data = data;
+                    this.token_data = data;
                     document.getElementById("login_password").value = '';
-                    authenticated_http_request("/users/me").then((request_data) => {
-                        console.log("got data:", request_data);
-                        login_msg.textContent = "";
-                        login_progress.innerHTML = "";
+                    this.authenticated_http_request("/users/me").then((request_data) => {
+                        // console.log("got data:", request_data);
+                        this.login_msg.textContent = "";
+                        this.login_progress.innerHTML = "";
                         const greeting = document.getElementById("greeting");
                         greeting.textContent = `Hallo, ${request_data.username}`;
-                        console.log("set greeting");
-                        modal_login_instance.close();
+                        // console.log("set greeting");
+                        this.modal_login_instance.close();
                     }).catch(err => {
                         console.error("login failed:", err);
-                        login_progress.innerHTML = "";
-                        login_msg.innerHTML = "<h6>Invalid username or password.</h6>";
+                        this.login_progress.innerHTML = "";
+                        this.login_msg.innerHTML = "<h6>Invalid username or password.</h6>";
                     });
-                    authenticated_http_request("/system/status").then(request_data => {
-                        const status_div = document.getElementById("system_status");
-                        let temp_data = '<div class="section">';
-                        for (let tmp_data of request_data.temperatures.coretemp) {
-                            temp_data += `
-                            <div>
-                                <label for='${tmp_data.label}'>${tmp_data.label}<i class="material-icons prefix">memory</i></label>
-                                <meter id='${tmp_data.label}' title="°C"  value="${tmp_data.current}" max="${tmp_data.critical}" min="0">${tmp_data.current}</meter>
-                                ${tmp_data.current} °C
-                                <br>
-                            </div>
-                            `
-                        };
-                        if (request_data.temperatures.nvidia_temp) {
-                            console.log(request_data.temperatures.nvidia_temp);
-                            temp_data += `
-                            <div>
-                                <label for='nvidia_gpu'>NVidia GPU <i class="material-icons prefix">memory</i></label>
-                                <meter id='nvidia_gpu' title="°C" value="${request_data.temperatures.nvidia_temp}" max="100" min="0">${request_data.temperatures.nvidia_temp}</meter>
-                                ${request_data.temperatures.nvidia_temp} °C
-                            </div>
-                            `;
-                        };
-                        temp_data += "</div>";
-                        status_div.innerHTML = temp_data;
-                    }).catch(err => {
-                        console.error("could not retrieve /system/status", err);
-                    })
-                    login_btn.disabled = false;
+                    update_sys_stats();
+                    this.sys_stat_timer = setInterval(update_sys_stats, 5000);
+                    this.login_btn.disabled = false;
                 }
             )
             .catch((err) => {
                 console.error("Aquiring token failed:", err);
-                login_msg.textContent = "Invalid username or password.";
-                login_btn.disabled = false;
+                this.login_msg.textContent = "Invalid username or password.";
+                this.login_btn.disabled = false;
             })
-            
+    }
+
+    async authenticated_http_request(url, method = "GET", request_data = {}) {
+        if (this.token_data === null) throw "token is null";
+        const myHeaders = new Headers();
+        myHeaders.append("accept", "application/json");
+        myHeaders.append("Authorization", `Bearer ${this.token_data.access_token}`)
+        let data = fetch(url,
+            {
+                method: method,
+                mode: 'cors',
+                cache: 'default',
+                headers: myHeaders,
+                body: (method.toUpperCase() == "GET") ? null : JSON.stringify(request_data),
+            }
+        ).then(response => {
+            if (!response.ok) {
+                throw Error(response.statusText);
+            }
+            return response.json();
+        }
+        ).catch(err => {
+            console.error("http request failed:", err);
+            throw err;
+        })
+        // console.log("end of authenticated http request")
+        return data;
+    }
+}
+
+function hit_key(key) {
+    auth.authenticated_http_request("/hitkey", "POST", {"key": key})
+    .then(data => {
+        console.log("sent key", key);
+    }).catch(err => console.error("sending", key, "failed:", err));
+    
+}
+
+function update_sys_stats() {
+    auth.authenticated_http_request("/system/status").then(request_data => {
+        const status_div = document.getElementById("system_status");
+        let temp_data = '<div class="section">';
+        for (let [i, v] of Object.entries(request_data.temperatures)) {
+            temp_data += `<div class="section"><div class="row"><span class="col 12s valign-center"><h6><i class="material-icons">developer_board</i> ${i}</h6></span></div>`;
+            v.forEach((t, n) => {
+                let temp_name = (t.label) ? t.label : `Temp_${n}`
+                console.log(t.current, typeof(t.current))
+                temp_data += `
+                    <div class="row">
+                        <span class="col s2">${temp_name}</span>
+                        <div class="col s2 meter">
+                            <span style="width: ${t.current/115 * 100}%"></span>
+                        </div>
+                        <div class="col s1 right-align">${t.current.toFixed(1)} °C</div>
+                        <br>
+                    </div>
+                    `;
+            });
+            temp_data += '</div>'
+        };
+        temp_data += "</div>";
+        status_div.innerHTML = temp_data;
+
+        const fans_div = document.getElementById("system_fans");
+        temp_data = '';
+        for (let [i, v] of Object.entries(request_data.fans)) {
+            temp_data += `<div class="section"><div class="row"><span class="col 12s valign-center"><h6><i class="material-icons">toys</i> ${i}</h6></span></div>`;
+            v.forEach((f, n) => {
+                let fan_name = (f.label) ? f.label : `Fan_${n}`;
+                temp_data += `
+                    <div class="row">
+                        <div class="col s1 m1">${fan_name}</div>
+                        <div class="col s1 m1 text-align"><span class="right">${f.current}</span></div>
+                        <div class="col s1 m1">RPM</div>
+                    </div>
+                    `;
+            })
+            temp_data += "</div>"
+        }
+        fans_div.innerHTML = temp_data;
+
+        const disk_div = document.getElementById('system_hdd');
+        temp_data = `
+            <div class="row">
+                <div class="col s2">Mountpoint</div>
+                <div class="col s2">Device</div>
+                <div class="col s2"><i class="material-icons">data_usage</i></div>
+                <div class="col s5">Usage</div>
+            </div>
+        `;
+        request_data.disk_usage.forEach((d, n) => {
+            temp_data += `
+                        <div class="row">
+                            
+                            <div class="col s2">${d.mountpoint}</div>
+                            <span class="col s2">${d.device}</span>
+                            <div class="col s1 meter">
+                                <span style="width: ${d.percent}%"></span>
+                            </div>
+                            <div class="col s5">${d.used_human.value} ${d.used_human.unit}/${d.total_human.value} ${d.total_human.unit} (${d.free_human.value} ${d.free_human.unit} free)</div>
+                        </div>
+                        `
+        });
+        disk_div.innerHTML = temp_data;
+
+        temp_data = '';
+        const res_div = document.getElementById('system_resources');
+        const m = request_data.memory_usage;
+        const c = request_data.cpu_usage;
+        const n_cpus = request_data.cpu_num;
+        let i;
+        for (i = 0; i < n_cpus; i++) {
+            temp_data += `
+                <div class="row">
+                    <i class="material-icons prefix col s1">memory</i>
+                    <span class="col s2">CPU ${i}</span>
+                    <div class="col s1 meter">
+                        <span style="width: ${c[i]/100}%"></span>
+                    </div>
+                    <div class="col s1 right-align">${c[i].toFixed(1)}%</div>
+                </div>
+            `;
+        }
+        temp_data += `
+                    <div class="row">
+                        <i class="material-icons prefix col s1">memory</i>
+                        <span class="col s2">RAM</span>
+                        <div class="col s1 meter">
+                            <span style="width: ${m.percent}%"></span>
+                        </div>
+                        <div class="col s2">${m.used_human.value} ${m.used_human.unit}/${m.total_human.value} ${m.total_human.unit} (${m.free_human.value} ${m.free_human.unit} free)</div>
+                    </div>
+                    `;
+        res_div.innerHTML = temp_data;
+        auth.login_btn.disabled = false;
+        })
+        .catch(err => {
+            console.error("could not retrieve /system/status", err);
+        })    
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    M.AutoInit();
+    auth = new Authorization();
+    let elems = document.querySelectorAll('.sidenav');
+    let instances = M.Sidenav.init(elems,
+        {
+            "edge": "left",
+        }
+    );
+
+    // wire up login button of modal login dialogue
+    auth.loginForm.addEventListener('submit', function (e) {
+        e.preventDefault();
+        auth.login(this);
     })
 });
