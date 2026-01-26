06b5b15e1ee8cb2ea4e3488a88db844d79e30118
Alexander Grothe <seahawk1986@gmx.de>
2026-01-26 01:17:58 +0100

    adapt to changed display information from playbook

--- src/yavdr_backend/routers/system.py
+++ src/yavdr_backend/routers/system.py
@@ -15,7 +15,11 @@ from pydantic import BaseModel
 from ruamel.yaml import YAML
 from sse_starlette import EventSourceResponse
 
-from yavdr_backend.interfaces.system_backend import YavdrSystemBackend, Status, YAVDR_BACKEND_INTERFACE
+from yavdr_backend.interfaces.system_backend import (
+    YavdrSystemBackend,
+    Status,
+    YAVDR_BACKEND_INTERFACE,
+)
 from yavdr_backend.models.xorg import XorgConfig
 # from interfaces.system_backend import YAVDR_BACKEND_INTERFACE
 
@@ -27,6 +31,7 @@ ANSIBLE_LOCK = Lock()
 class Playbook(BaseModel):
     playbook: str
     options: Mapping[str, Any]
+
 
 @router.post("/system/playbook/rescan_displays")
 async def run_playbook(request: Request):
@@ -65,64 +70,67 @@ async def run_playbook(request: Request):
                 else:
                     try:
                         data = json.loads(message)
-                        if data.get('event', {}).get('runner_ident') == runner_ident or data.get('status', {}).get('runner_ident'):
+                        if data.get("event", {}).get(
+                            "runner_ident"
+                        ) == runner_ident or data.get("status", {}).get("runner_ident"):
                             yield message
                     except json.JSONDecodeError as e:
                         print(e, file=sys.stderr)
                         # yield f"{e}: {message}"
+
     return EventSourceResponse(event_generator(), send_timeout=5)
 
-@router.get('/system/xorg_config')
+
+@router.get("/system/xorg_config")
 async def get_xorg_config() -> XorgConfig:
-    config_file = Path('/etc/yavdr/display_config.yml')
+    config_file = Path("/etc/yavdr/display_config.yml")
     yaml = YAML(typ="safe")
     try:
         with open(config_file) as f:
-            data: dict[str, Any] = yaml.load(f) # pyright: ignore[reportUnknownVariableType, reportUnknownMemberType]
+            data: dict[str, Any] = yaml.load(f)  # pyright: ignore[reportUnknownVariableType, reportUnknownMemberType]
     except IOError:
-        fallback_config_file = Path('/etc/ansible/facts.d/xorg_config.fact')
+        fallback_config_file = Path("/etc/ansible/facts.d/monitor_config.fact")
         print(f"fallback to {fallback_config_file=}")
         try:
             with open(fallback_config_file) as f:
-                data = json.load(f).get('xorg_config', {})
+                data = json.load(f).get("monitor_config", {})
         except Exception:
-            logging.exception("could not load xorg_config, please rescan displays")
+            logging.exception("could not load monitor_config, please rescan displays")
             raise
 
     print(f"{data=}")
     config: XorgConfig = XorgConfig.model_validate(data)
     return config
 
-@router.get('/system/xorg_facts')
-async def get_xorg_facts() -> FileResponse:
-    return FileResponse('/etc/ansible/facts.d/xorg.fact',
-                        headers={"Cache-Control": "no-store"})
 
-
-@router.get('/system/xrandr_facts')
+@router.get("/system/display_outputs")
 async def get_xrandr_facts() -> FileResponse:
-    return FileResponse('/etc/ansible/facts.d/xrandr.fact',
-                        headers={"Cache-Control": "no-store"})
+    return FileResponse(
+        "/etc/ansible/facts.d/display_outputs.fact",
+        headers={"Cache-Control": "no-store"},
+    )
 
-@router.get('/system/drm_facts')
-async def get_drm_facts() -> FileResponse:
-    return FileResponse('/etc/ansible/facts.d/drm.fact',
-                        headers={"Cache-Control": "no-store"})
 
-@router.get('/system/xorg_config')
+@router.get("/system/display_config")
 async def get_xorg_config_facts() -> FileResponse:
-    return FileResponse('/etc/ansible/facts.d/xorg_config.fact',
-                        headers={"Cache-Control": "no-store"})
+    return FileResponse(
+        "/etc/ansible/facts.d/display_config.fact",
+        headers={"Cache-Control": "no-store"},
+    )
 
-@router.post('/system/xorg_config')
+
+@router.post("/system/xorg_config")
 async def set_xorg_confg(config: XorgConfig, request: Request) -> EventSourceResponse:
     print("post xorg_config:", config)
+
     async def event_generator():
         with closing(sdbus.sd_bus_open_system()) as system_bus:
             backend_connection = YavdrSystemBackend(system_bus).new_proxy(
                 YAVDR_BACKEND_INTERFACE, "/", system_bus
             )
-            await backend_connection.write_display_configuration(config.model_dump_json())
+            await backend_connection.write_display_configuration(
+                config.model_dump_json()
+            )
             job_uuid = await backend_connection.set_display_configuration()
             runner_ident = None
             async for event in backend_connection.ansible_event:
@@ -147,7 +155,9 @@ async def set_xorg_confg(config: XorgConfig, request: Request) -> EventSourceRes
                 else:
                     try:
                         data = json.loads(message)
-                        if data.get('event', {}).get('runner_ident') == runner_ident or data.get('status', {}).get('runner_ident'):
+                        if data.get("event", {}).get(
+                            "runner_ident"
+                        ) == runner_ident or data.get("status", {}).get("runner_ident"):
                             yield message
                     except json.JSONDecodeError as e:
                         print(e, file=sys.stderr)
